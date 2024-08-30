0ee40e9c4093050836f4b6bdf4e9c372ff070c3d
chaow porkaew <pchaowmobile@gmail.com>
2024-08-30 04:09:47 +0000

    book

--- maechan/maechan_streetcutout/front-end/streetcutoutfront/tailwind.config.cjs
+++ maechan/maechan_streetcutout/front-end/streetcutoutfront/tailwind.config.cjs
@@ -1,12 +1,18 @@
-const {nextui} = require("@nextui-org/react");
+const { nextui } = require("@nextui-org/react");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}","./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'custom-gradient': 'linear-gradient(to right, #6399ab, #cde8f1)', // Gradient 
+      },
+    },
   },
   plugins: [nextui()],
 }
