890b153e1b7514dffaeb2918619c0386c65df776
chaow porkaew <pchaowmobile@gmail.com>
2024-09-10 05:54:34 +0000

    a

--- maechan/maechan_streetcutout/front-end/streetcutoutfront/src/pages/Streetcutout/RequeststreetcutouttaxCreate.tsx
+++ maechan/maechan_streetcutout/front-end/streetcutoutfront/src/pages/Streetcutout/RequeststreetcutouttaxCreate.tsx
@@ -1,10 +1,11 @@
 import { BreadcrumbItem, Breadcrumbs, Input, Button, Select, SelectItem, Table, TableBody, TableCell, TableColumn, TableHeader, TableRow, Autocomplete, AutocompleteItem } from "@nextui-org/react"
-import { useContext, useEffect, useMemo, useRef, useState } from "react"
+import { SetStateAction, useContext, useEffect, useMemo, useRef, useState } from "react"
 import { FaHome, FaPlus } from "react-icons/fa"
 import { Link, useNavigate } from "react-router-dom"
 import { IRequestStreetcutout, IAmphure, IBusiness, IHouse, IProvince, IRequestDetail, IRequestLicense, IRequestLicenseType, ITambon, IUserProfile } from "../../interfaces"
 import { FrappeConfig, FrappeContext } from "frappe-react-sdk"
 import { useAlertContext } from "../../providers/AlertProvider"
+import useSWR from 'swr';
 
 export default function RequeststreetcutouttaxCreate() {
 
@@ -25,31 +26,23 @@ export default function RequeststreetcutouttaxCreate() {
 
 
     let [createForm, setCreateForm] = useState({} as IRequestStreetcutout)
-    let { call } = useContext(FrappeContext) as FrappeConfig
 
     const [isLoading, setIsLoading] = useState(true)
     const [invalid, setInvalid] = useState(true)
+    const [isSaved, setIsSaved] = useState(false);
 
     console.log(invalid)
 
 
-    const updateForm = async (key: string, value: string | number) => {
-
+    const updateForm = (key: string, value: string | number) => {
         let createFormValue = {
             ...createForm,
             [key]: value
         } as IRequestStreetcutout
 
-    }
+        setCreateForm(createFormValue)
+    };
 
-
-    useEffect(() => {
-
-    }, [])
-
-    const [error, setError] = useState({
-        result: null,
-    })
 
     const save = async () => {
         let response = await call.post("maechan.maechan_streetcutout.doctype.requeststreetcutouttax.requeststreetcutouttax.first_step_requeststreetcutouttax", {
@@ -61,79 +54,6 @@ export default function RequeststreetcutouttaxCreate() {
         navigate(`/StreetcutoutRequest/${response.message.name}/edit`)
 
     }
-
-    const validate = () => {
-
-        if (createForm.streetcutout_img) {
-            return true
-        }
-
-        alert.showError("กรุณาอัพโหลดตัวอย่างป้าย")
-
-        return false
-    }
-    const UploadButton = ({ doc }: { doc: IRequestStreetcutout }) => {
-
-        const { file } = useContext(FrappeContext) as FrappeConfig
-
-        const inputFile = useRef<HTMLInputElement>(null)
-
-        const [isUploading, setIsUploading] = useState(false)
-        const openInputFile = () => {
-            inputFile?.current?.click();
-
-        }
-
-        const uploadFile = async (e: any) => {
-            setIsUploading(true)
-            let myFile = e.target.files[0]
-            const fileArgs = {
-                "isPrivate": false,
-                "folder": "home/RequestStreetcutoutAttachment",
-            }
-
-            file.uploadFile(
-                myFile,
-                fileArgs,
-                /** Progress Indicator callback function **/
-                (completedBytes, totalBytes) => console.log(Math.round((completedBytes / (totalBytes ?? completedBytes + 1)) * 100), " completed")
-            )
-                .then((response) => {
-                    console.log("File Upload complete")
-                    let fileResponse = response.data.message
-                    console.log(response)
-                    call.post("maechan.maechan_streetcutout.doctype.requeststreetcutouttax.requeststreetcutouttax.first_step_requeststreetcutouttax", {
-                    }).catch(e => alert.showError(JSON.stringify(e)))
-                })
-                .catch(e => console.error(e))
-
-
-        }
-        const siteName = import.meta.env.VITE_FRAPPE_URL ?? window.origin
-
-
-        if (doc.streetcutout_img) {
-            return (
-                <div className="flex flex-row gap-3">
-                    <Button isLoading={isUploading} onClick={() => { window.open(`${siteName}/${doc.streetcutout_img}`) }} color="primary">
-                        ดูรูป
-                    </Button>
-                    <Button isLoading={isUploading}  color="danger">ลบ</Button>
-                </div>
-            )
-        } else {
-            return (
-                <div>
-                    <Button isLoading={isUploading} color="primary" onClick={openInputFile}>อัพโหลดตัวอย่างป้าย</Button>
-                    <input type="file" id="file" onChange={uploadFile} ref={inputFile} style={{ display: "none" }} />
-
-                </div>
-            )
-        }
-
-
-    }
-    
 
     return (
         <div className="flex flex-col">
@@ -167,8 +87,11 @@ export default function RequeststreetcutouttaxCreate() {
                     name="streetcutout_count_requeststreetcutouttax"
                     onChange={(e) => updateForm(e.target.name, Number(e.target.value))}
                     type="number"
-                    min="0"
-                    label="จำนวนป้าย" />
+                    min="1"
+                    step="1"
+                    label="จำนวนป้าย"
+                />
+
 
                 <Select
                     label="ขนาดของป้าย"
@@ -178,19 +101,8 @@ export default function RequeststreetcutouttaxCreate() {
                     <SelectItem key="ขนาดของป้าย" >120x240เซนติเมตร</SelectItem>
                 </Select>
             </div>
-            <div className="grid grid-cols-3 gap-3 mb-3">
-                <UploadButton doc={createForm} />
-            </div>
-
-            <div className="flex flex-row lg:w-[50%] text-md mb-3">
-                testที่อยู่สถานประกอบการ (หากไม่พบบ้านเลขที่กรุณาแจ้งผู้ดูแลระบบ)
-            </div>
-            <div className="grid gap-3 mb-3 grid-row sm:grid-cols-3">
-                test
-            </div>
-
             <div className="flex flex-row lg:w-[50%] text-xl mb-3">
-                <Button disabled={invalid} className={`mr-3`} color="primary" isDisabled={invalid} onClick={save}>บันทึกและต่อไป</Button>
+                <Button className={`mr-3`} color="primary" onClick={save}>บันทึกและต่อไป</Button>
                 <Button className="mr-3" onClick={() => { navigate("/StreetcutoutRequest") }} color="default">ยกเลิก</Button>
             </div>
         </div>
